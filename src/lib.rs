b88783367468e8caa033fe57659c26cbefc38cae
zachary nickels <zach.n500@gmail.com>
2021-10-11 19:12:53 -0600

    log to file, filter unique, chunking of iterators, remove recursion

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,68 @@
 use rayon::prelude::*;
 use reqwest::StatusCode;
 use select::{document::Document, predicate::Name};
+use std::fs::File;
 use std::io::{self, BufRead, Write};
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
 
-pub fn run(urls: Vec<String>) {
-    let url_iter = urls.par_iter();
+pub fn run() {
+    let (tx, rx): (Sender<String>, Receiver<String>) = mpsc::channel();
+    let mut urls: Vec<String> = Vec::new();
+    let mut output_file = File::create("output.txt").unwrap();
 
-    let mut fetched = Vec::new();
-    url_iter
-        .map(|url| match reqwest::blocking::get(url) {
-            Ok(resp) => crawl_page(resp),
-            Err(_) => Vec::with_capacity(0),
-        })
-        .collect_into_vec(&mut fetched);
+    println!(
+        "Enter a series of URLs by pressing ENTER after each. \n\
+        To stop submitting new URLs, simply input exclusively ENTER."
+    );
+    while let Some(url) = read_url() {
+        urls.push(url);
+    }
 
-    for result_list in fetched {
-        run(result_list);
+    println!("CRAWLER STARTING.");
+
+    let _ = std::thread::spawn(move || loop {
+        if let Some(input) = read() {
+            tx.send(input).unwrap();
+        }
+    });
+    'outer: loop {
+        let url_iter = urls.chunks(5);
+
+        let mut fetched = Vec::new();
+        for chunk in url_iter {
+            if let Ok(command) = rx.try_recv() {
+                println!("Got command: {}", command);
+
+                if command == "stop" || command == "quit" {
+                    println!("CRAWLER STOPPING.");
+                    break 'outer;
+                }
+            }
+
+            fetched.append(
+                &mut chunk
+                    .par_iter()
+                    .flat_map(|link| {
+                        if let Ok(resp) = reqwest::blocking::get(link) {
+                            crawl_page(resp)
+                        } else {
+                            Vec::new()
+                        }
+                    })
+                    .filter(|link| !urls.contains(link) && !fetched.contains(link))
+                    .collect::<Vec<String>>(),
+            );
+        }
+
+        println!("CRAWLER FOUND {} LINKS", fetched.len());
+
+        for link in &fetched {
+            let _ = output_file.write(link.as_bytes());
+            let _ = output_file.write(b"\n");
+        }
+
+        urls = fetched;
     }
 }
 
@@ -26,16 +73,28 @@ pub fn crawl_page(resp: reqwest::blocking::Response) -> Vec<String> {
         Document::from(text.as_str())
             .find(Name("a")) // Find all a nodes
             .filter_map(|n| match n.attr("href") {
-                // Filter each node for nodes that have the href attr and map to a new iterator
                 Some(link) => {
-                    println!("Got link: {}", link);
-                    Some(link.to_owned())
+                    if link.contains("http://") || link.contains("https://") {
+                        Some(link.to_owned())
+                    } else {
+                        None
+                    }
                 } // for every node that has the attr, allocate the node's value to a String
                 None => None, // or if node didnt have the href attr, let it fail the map operation
             })
             .collect()
     } else {
         Vec::with_capacity(0)
+    }
+}
+
+fn read() -> Option<String> {
+    let stdin = io::stdin();
+    let line = stdin.lock().lines().next();
+
+    match line {
+        Some(Ok(input)) => Some(input),
+        _ => None,
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,7 @@
-use shells;
+
+
+use crawl;
 
 fn main() {
-    let mut urls = Vec::<String>::with_capacity(5);
-    println!(
-        "Enter a series of URLs by pressing ENTER after each. \n\
-        To stop submitting new URLs, simply input exclusively ENTER."
-    );
-    while let Some(url) = shells::read_url() {
-        urls.push(url);
-    }
-
-    shells::run(urls);
+    crawl::run();
 }
